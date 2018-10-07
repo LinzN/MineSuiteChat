e540e8b39283cd4285565cb96cdcf3a8ec1152cd
Niklas <niklas.linz@hotmail.com>
2018-10-07 13:27:27 +0200

    Remove old stuff

--- src/main/java/de/linzn/mineSuite/chat/api/ChatManager.java
+++ src/main/java/de/linzn/mineSuite/chat/api/ChatManager.java
@@ -17,23 +17,6 @@ import org.bukkit.entity.Player;
 
 public class ChatManager {
 
-	/*
-	public static void sendguildChatMsg(String gname, String text) {
-		Guild guild = DataBaseHASHApi.getGuildHASH(gname);
-		if (guild != null) {
-			Bukkit.getConsoleSender().sendMessage(guild.getGuildName() + "-> " + text);
-			for (GuildPlayer gp : DataBaseHASHApi.getOnlineGuildPlayers()) {
-				if (gp.getGuild() == guild) {
-					gp.sendText(text);
-				}
-
-			}
-
-		}
-
-	}
-	*/
-
     public static void sendStaffChatMsg(String text) {
         for (Player p : Bukkit.getOnlinePlayers()) {
             if (p.hasPermission("mineSuite.chat.staff")) {
